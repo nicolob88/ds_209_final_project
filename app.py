ad16b28d071e6fd68bb8ee95c12a19b1e64b1dc7
Megan Nguyen <107975658+meganguyen19@users.noreply.github.com>
2023-12-08 13:29:08 -0600

    Megan Flask Website

--- app.py
+++ app.py
@@ -5,26 +5,27 @@ import pandas as pd
 
 # Flask routes
 @app.route('/')
+@app.route('/index.html')
 def home():
     return render_template('index.html', css=url_for('static', filename='style.css'))
 
-@app.route('/energyoverview')
+@app.route('/energyoverview.html')
 def energyoverview():
     return render_template('energyoverview.html', css=url_for('static', filename='style.css'))
 
-@app.route('/energyprojections')
+@app.route('/energyprojections.html')
 def energyprojections():
     return render_template('energyprojections.html', css=url_for('static', filename='style.css'))
 
-@app.route('/cleanenergy')
+@app.route('/cleanenergy.html')
 def cleanenergy():
     return render_template('cleanenergy.html', css=url_for('static', filename='style.css'))
 
-@app.route('/datasources')
+@app.route('/datasources.html')
 def datasources():
     return render_template('datasources.html', css=url_for('static', filename='style.css'))
 
-@app.route('/contact')
+@app.route('/contact.html')
 def contact():
     return render_template('contact.html', css=url_for('static', filename='style.css'))
 
